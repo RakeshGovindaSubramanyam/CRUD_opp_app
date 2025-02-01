15514f27d03c7528847caecda83ed5e0f9107218
Rakesh Govinda Subramanyam <govindasubramanyamrakesh1999@gmail.com>
2025-01-31 19:02:38 -0500

    models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
